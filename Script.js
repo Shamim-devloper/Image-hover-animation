8fec1a1058e856a5bbc1021192a55aaf532b0467
Shamim-devloper <100067687+Shamim-devloper@users.noreply.github.com>
2022-02-22 19:33:35 +0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,6 @@
+setTimeout(()=>{
+       document.getElementById("hide").style.display="block";
+     },650);
+ setTimeout(()=>{
+       document.getElementById("hide").style.display="none";
+     },7650);
