028b1d336696de14007d8a821b9bfd67fa4d7201
Shamim-devloper <100067687+Shamim-devloper@users.noreply.github.com>
2022-02-22 19:42:52 +0600

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-setTimeout(()=>{
-       document.getElementById("hide").style.display="block";
-     },650);
- setTimeout(()=>{
-       document.getElementById("hide").style.display="none";
-     },7650);
